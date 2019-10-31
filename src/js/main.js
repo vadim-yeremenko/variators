dd3e151747fc876b39ade79be52f1811eaec002d
Irina <irina.grigoryan.stratis@gmail.com>
2019-10-31 22:04:39 +0200

    Added service block

--- src/js/main.js
+++ src/js/main.js
@@ -27,4 +27,20 @@ document.addEventListener('DOMContentLoaded', () => {
 	    arrows: false,
         appendDots: '.banner__wrapper'
 	})
+
+//service slider	
+    $(".slider-service").slick({
+    	// autoplay: true,
+  		autoplaySpeed: 4000,
+	    speed: 1500,	   
+	    slidesToScroll: 1,
+	    slidesToShow: 1,
+	    infinite: true,
+	    centerMode: false,
+	    variableWidth: false,
+	    focusOnSelect: true,     
+	    dots: true,  
+	    arrows: false,
+        // appendDots: '.banner__wrapper'
+	})
 });
